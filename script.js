266d1b3416dd2d814bda36bb03518a18d9742f17
Shandira <sduxe23@gmail.com>
2020-09-19 21:29:27 -0700

    quotes now generate upon refresh

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const  quoteContainer = document.getElementById('quote-container');
+const  quoteText = document.getElementById('quote');
+const  authorText = document.getElementById('author');
+const  twitterBtn = document.getElementById('twitter');
+const  newQuoteBtn = document.getElementById('new-quote');
+
 // Get quote from API
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
@@ -5,10 +11,12 @@ async function getQuote() {
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+        authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
+        //console.log(data);
     } catch (error) {
         getQuote();
-        console.log('whoops, no quote', + error);
+        //console.log('whoops, no quote', + error);
     }
 }
 
